1de2e2012fa480acaa4707978cae417341f5dcf7
Allison Usher <allyjay317@gmail.com>
2020-04-24 18:46:55 -0700

    completed tallyUpDonations

--- index.js
+++ index.js
@@ -294,8 +294,9 @@ function getRunnersByTShirtSize(runners, tShirtSize) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+function tallyUpDonations(runners) {
+  let total = runners.reduce((res, s) => {return res += s.donation}, 0);
+  return total;
 }
 
 /////////////// CLOSURES ///////////////
